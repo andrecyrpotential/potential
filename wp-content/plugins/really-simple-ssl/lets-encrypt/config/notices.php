298a822929ca6a8aedb2a064973d0aed0c78e120
edupotential <andre.cyr@edupotential.ca>
2023-06-14 21:05:32 -0400

    Commit 07

--- wp-content/plugins/really-simple-ssl/lets-encrypt/config/notices.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_filter() in /Users/macos/Documents/potential/app/public/wp-content/plugins/really-simple-ssl/lets-encrypt/config/notices.php:154
-Stack trace:
-#0 {main}
-  thrown in <b>/Users/macos/Documents/potential/app/public/wp-content/plugins/really-simple-ssl/lets-encrypt/config/notices.php</b> on line <b>154</b><br>
