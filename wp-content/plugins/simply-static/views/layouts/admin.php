a2adebbfa7cd2c491102e9a39324b1b6b4abd8e6
edupotential <andre.cyr@edupotential.ca>
2023-07-26 10:21:16 -0400

    Commit 27

--- wp-content/plugins/simply-static/views/layouts/admin.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Using $this when not in object context in /Users/macos/Documents/potential/app/public/wp-content/plugins/simply-static/views/layouts/admin.php:5
-Stack trace:
-#0 {main}
-  thrown in <b>/Users/macos/Documents/potential/app/public/wp-content/plugins/simply-static/views/layouts/admin.php</b> on line <b>5</b><br>
